9d73bea219e6ec1d6ac9d146d68a2d1f9de1a52e
Ben Smith <cmdshiftd@gmail.com>
2025-12-14 13:47:02 +0000

    updated so-auto.iso

--- so-auto/so-manager.sh
+++ so-auto/so-manager.sh
@@ -1,4 +1,4 @@
-cp /home/tester/SecurityOnion/setup/so-whiptail setup/so-whiptail.bak
+cp /home/tester/SecurityOnion/setup/so-whiptail /home/tester/SecurityOnion/setup/so-whiptail.bak
 cp so-whiptail.manager /home/tester/SecurityOnion/setup/so-whiptail
 chmod +x /home/tester/SecurityOnion/setup/so-whiptail
 sudo bash /home/tester/SecurityOnion/setup/so-setup iso
